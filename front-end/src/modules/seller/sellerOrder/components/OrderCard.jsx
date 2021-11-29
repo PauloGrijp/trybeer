fd151697901014c029e91466beb840998776d2bc
renanpbraga <renanpbraga@gmail.com>
2021-11-29 09:03:37 -0300

    feat(seller): style order card

--- front-end/src/modules/seller/sellerOrder/components/OrderCard.jsx
+++ front-end/src/modules/seller/sellerOrder/components/OrderCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { formatedDate } from '../../../../helpers/functions';
+import { formatedDate, statusOrderStyle } from '../../../../helpers/functions';
+import './OrderCard.module.scss';
 
 export default function OrderCard({ order }) {
   const address = `${order.deliveryAddress}, ${order.deliveryNumber}`;
   return (
-    <Link className="d-flex w-50" to={ `/seller/orders/${order.id}` }>
-      <div className="m-4">
+    <Link className="card" to={ `/seller/orders/${order.id}` }>
+      <div className="order">
         <p>Pedido</p>
         <p
           data-testid={ `seller_orders__element-order-id-${order.id}` }
@@ -15,34 +16,35 @@ export default function OrderCard({ order }) {
           { order.id }
         </p>
       </div>
-      <div className="d-flex justify-content-between m-4">
+      <div className="status">
         <p
+          style={ { color: statusOrderStyle(order.status) } }
           data-testid={ `seller_orders__element-delivery-status-${order.id}` }
         >
           { order.status }
         </p>
-        <div className="mx-4">
-          <p
-            data-testid={ `seller_orders__element-order-date-${order.id}` }
+      </div>
+      <div className="detail">
+        <p
+          data-testid={ `seller_orders__element-order-date-${order.id}` }
+        >
+          { formatedDate(new Date(order.saleDate)) }
+        </p>
+        <p>
+          R$
+          <span
+            data-testid={ `seller_orders__element-card-price-${order.id}` }
           >
-            { formatedDate(new Date(order.saleDate)) }
-          </p>
-          <p>
-            R$
-            <span
-              data-testid={ `seller_orders__element-card-price-${order.id}` }
-            >
-              { order.totalPrice }
-            </span>
-          </p>
-          <p>
-            <span
-              data-testid={ `seller_orders__element-card-price-${order.id}` }
-            >
-              { address }
-            </span>
-          </p>
-        </div>
+            { order.totalPrice }
+          </span>
+        </p>
+        <p>
+          <span
+            data-testid={ `seller_orders__element-card-price-${order.id}` }
+          >
+            { address }
+          </span>
+        </p>
       </div>
     </Link>
   );
